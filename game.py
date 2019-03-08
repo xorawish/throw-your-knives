389d19b216a9abf8e27503acd3e2e0108980fc0c
xorawish <xorawish@gmail.com>
2019-03-08 23:45:07 +0700

    show knife

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import arcade
+from models import World, Knife
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
@@ -9,8 +10,30 @@ class GameWindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLACK)
 
+        self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.knife_sprite = ModelSprite('images/knife.png', model=self.world.knife)
+
+    def update(self, delta):
+        self.world.update(delta)
+
     def on_draw(self):
         arcade.start_render()
+
+        self.knife_sprite.draw()
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
+
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
 
 def main():
     window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
---
+++ models.py
@@ -0,0 +1,18 @@
+class Knife:
+    def __init__(self, world, x, y):
+        self.world = world
+        self.x = x
+        self.y = y
+
+    def update(self, delta):
+        pass
+
+class World:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+        self.knife = Knife(self, width // 2, height // 2)
+
+    def update(self, delta):
+        self.knife.update(delta)
