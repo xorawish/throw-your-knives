96815ac5245c197f0f6358474e8eeedcfc572824
xorawish <xorawish@gmail.com>
2019-03-08 16:30:40 +0700

    show background

---
+++ game.py
@@ -0,0 +1,21 @@
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+class GameWindow(arcade.Window):
+    def __init__(self, width, height):
+        super().__init__(width, height)
+
+        arcade.set_background_color(arcade.color.BLACK)
+
+    def on_draw(self):
+        arcade.start_render()
+
+def main():
+    window = GameWindow(SCREEN_WIDTH, SCREEN_HEIGHT)
+    arcade.set_window(window)
+    arcade.run()
+
+if __name__ == '__main__':
+    main()
