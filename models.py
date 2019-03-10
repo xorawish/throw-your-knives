30a7bd557db0b749c7590b2f9eb79acf9e8c763d
xorawish <xorawish@gmail.com>
2019-03-11 02:52:45 +0700

    stab the target

--- models.py
+++ models.py
@@ -11,9 +11,15 @@ class Knife:
     def throw(self):
         self.THROWN = True
 
+    def stab(self):
+        self.THROWN = False
+
     def update(self, delta):
         if self.THROWN:
             self.y += self.THROW_VELOCITY
+
+    def hit(self, other, hit_size):
+        return (abs(self.x - other.x) <= hit_size) and (abs(self.y - other.y) <= hit_size)
 
 class Target:
     def __init__(self, world, x, y):
@@ -34,6 +40,8 @@ class World:
 
     def update(self, delta):
         self.knife.update(delta)
+        if self.knife.hit(self.target, 60):
+            self.knife.stab()
 
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.SPACE:
